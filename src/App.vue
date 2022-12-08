5f57254fdc6e152aca098aa1e28308585ca1cc28
Eduardo Córdoba <eduardo.cordoba.j@gmail.com>
2022-12-08 11:15:13 +0100

    lab setup and done, no style at all

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue';
+import Footer from './components/Footer.vue';
+</script>
+
+<template>
+  <Navbar />
+  <HelloWorld msg="Vite + Vue" />
+  <Footer />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        Navbar : {{ (2 + 2) }}<br />
+        Return string: {{ returnString() }}<br />
+        Show text: <span v-if="showText">Depending of condition!</span><br />
+        <button @click="toggleText()">Show text above!</button>
+    </div>
+    <div v-for="data in dataObjectArray" :key="data.title">
+        <span>{{ data.title }}</span><br />
+        <span>{{ data.description }}</span><br />
+        <span>{{ data.content }}</span><br />
+    </div>
+    <div v-bind:class="[changeBackground ? 'background-1' : 'background-2']">
+        <button @click="changeColor()">Change color</button>
+    </div>
+</template>
+<script setup>
+import { ref } from 'vue';
+
+const stringReturned = ref('Hola');
+let showText = ref(false);
+let changeBackground = ref(false);
+const dataObjectArray = [{ title: 'Titulo1', description: 'Descripcion1', content: 'Contenido1' },
+{ title: 'Titulo2', description: 'Descripcion2', content: 'Contenido2' },
+{ title: 'Titulo3', description: 'Descripcion3', content: 'Contenido3' },]
+
+function returnString() {
+    return 'Hi, this is a string returned by a function'
+}
+function toggleText() {
+    showText.value = !showText.value;
+}
+function changeColor() {
+    changeBackground.value = !changeBackground.value
+}
+</script>
+<style>
+.background-1 {
+    background-color: aqua;
+}
+
+.background-2 {
+    background-color: blueviolet;
+}
+</style>
